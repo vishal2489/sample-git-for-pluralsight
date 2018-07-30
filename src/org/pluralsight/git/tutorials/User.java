03b31f58c02d75bde96429482f0dde0055a07c19
Vishal <iste.mca.vishal@gmail.com>
2018-07-30 06:31:58 +0530

    created getter setter

--- src/org/pluralsight/git/tutorials/User.java
+++ src/org/pluralsight/git/tutorials/User.java
@@ -1,5 +1,14 @@
 package org.pluralsight.git.tutorials;
 
 public class User {
+	private  String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
