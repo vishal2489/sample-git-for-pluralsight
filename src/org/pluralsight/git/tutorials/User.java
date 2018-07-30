19e56e433e745fab0a0418d1dd5295f41fc6d962
Vishal <iste.mca.vishal@gmail.com>
2018-07-30 06:39:58 +0530

    added email getter setter

--- src/org/pluralsight/git/tutorials/User.java
+++ src/org/pluralsight/git/tutorials/User.java
@@ -2,6 +2,7 @@ package org.pluralsight.git.tutorials;
 
 public class User {
 	private  String name;
+	private String email;
 
 	public String getName() {
 		return name;
@@ -9,6 +10,14 @@ public class User {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 }
